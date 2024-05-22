99c45cfe17cf82fd25cb5c602bbd1a6c1c113726
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-05-22 20:53:21 +0800

    Update API coba lagi

--- server/index.js
+++ server/index.js
@@ -44,137 +44,137 @@ app.get("/api/v1/cars", (req, res)=>{
     });
 });
 
-app.get("/api/v1/cars/:id", (req, res) => {
-    const carId = req.params.id;
-    database.query("SELECT * FROM cars WHERE id = ?", [carId], (err, result) => {
-      if (err) throw err;
-      if (result.length === 0) {
-        return res.status(404).json({ success: false, message: "Car not found" });
-      }
-      res.json({
-        status: 200,
-        success: true,
-        data: result[0],
-      });
-    });
-  });
+// app.get("/api/v1/cars/:id", (req, res) => {
+//     const carId = req.params.id;
+//     database.query("SELECT * FROM cars WHERE id = ?", [carId], (err, result) => {
+//       if (err) throw err;
+//       if (result.length === 0) {
+//         return res.status(404).json({ success: false, message: "Car not found" });
+//       }
+//       res.json({
+//         status: 200,
+//         success: true,
+//         data: result[0],
+//       });
+//     });
+//   });
 
-// CRUD
-// MENAMBAHKAN MOBIL ATAU FITUR CREATE ATAU API UNTUK CREATE MOBIL
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Direktori penyimpanan file
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname); // Nama file yang disimpan
-    }
-  });
-  const upload = multer({ storage: storage });
+// // CRUD
+// // MENAMBAHKAN MOBIL ATAU FITUR CREATE ATAU API UNTUK CREATE MOBIL
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, 'uploads/'); // Direktori penyimpanan file
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname); // Nama file yang disimpan
+//     }
+//   });
+//   const upload = multer({ storage: storage });
 
-// ======================CREATE START ==============================
+// // ======================CREATE START ==============================
 
-app.post("/api/v1/cars", upload.single('image'), (req, res) => {
-    const { make, model, year, color, registration_number, daily_rate, status } = req.body;
-    const image_url = `/uploads/${req.file.filename}`;
+// app.post("/api/v1/cars", upload.single('image'), (req, res) => {
+//     const { make, model, year, color, registration_number, daily_rate, status } = req.body;
+//     const image_url = `/uploads/${req.file.filename}`;
   
-    database.query(
-      "INSERT INTO cars (make, model, year, color, registration_number, daily_rate, image_url, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-      [make, model, year, color, registration_number, daily_rate, image_url, status],
-      (err, result) => {
-        if (err) {
-          // Hapus file yang sudah diunggah jika terjadi kesalahan saat menyimpan ke database
-          fs.unlinkSync(req.file.path);
-          console.log('error', err);
-          return res.status(500).json({ success: false, message: "Gagal menambahkan mobil", error: err });
-        }
-        res.status(201).json({
-          success: true,
-          message: "Mobil berhasil ditambahkan",
-          data: { car_id: result.insertId, make, model, year, color, registration_number, daily_rate, image_url, status },
-        });
-      }
-    );
-  });
-// ======================CREATE END ==============================
+//     database.query(
+//       "INSERT INTO cars (make, model, year, color, registration_number, daily_rate, image_url, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+//       [make, model, year, color, registration_number, daily_rate, image_url, status],
+//       (err, result) => {
+//         if (err) {
+//           // Hapus file yang sudah diunggah jika terjadi kesalahan saat menyimpan ke database
+//           fs.unlinkSync(req.file.path);
+//           console.log('error', err);
+//           return res.status(500).json({ success: false, message: "Gagal menambahkan mobil", error: err });
+//         }
+//         res.status(201).json({
+//           success: true,
+//           message: "Mobil berhasil ditambahkan",
+//           data: { car_id: result.insertId, make, model, year, color, registration_number, daily_rate, image_url, status },
+//         });
+//       }
+//     );
+//   });
+// // ======================CREATE END ==============================
 
 
-  app.put("/api/v1/cars/:id", (req, res) => {
-    const carId = req.params.id;
-    const { make, model, year, registration_number, daily_rate, status, image_url } = req.body;
+//   app.put("/api/v1/cars/:id", (req, res) => {
+//     const carId = req.params.id;
+//     const { make, model, year, registration_number, daily_rate, status, image_url } = req.body;
   
-    database.query(
-      "UPDATE cars SET make = ?, model = ?, year = ?, registration_number = ?, daily_rate = ?, status = ?, image_url = ? WHERE id = ?",
-      [make, model, year, registration_number, daily_rate, status, image_url, carId],
-      (err, result) => {
-        if (err) throw err;
-        if (result.affectedRows === 0) {
-          return res.status(404).json({ success: false, message: "Car not found" });
-        }
-        res.json({
-          success: true,
-          message: "Car updated successfully",
-        });
-      }
-    );
+//     database.query(
+//       "UPDATE cars SET make = ?, model = ?, year = ?, registration_number = ?, daily_rate = ?, status = ?, image_url = ? WHERE id = ?",
+//       [make, model, year, registration_number, daily_rate, status, image_url, carId],
+//       (err, result) => {
+//         if (err) throw err;
+//         if (result.affectedRows === 0) {
+//           return res.status(404).json({ success: false, message: "Car not found" });
+//         }
+//         res.json({
+//           success: true,
+//           message: "Car updated successfully",
+//         });
+//       }
+//     );
 
-  });
+//   });
 
-  app.delete("/api/v1/cars/:id", (req, res) => {
-    const carId = req.params.id;
+//   app.delete("/api/v1/cars/:id", (req, res) => {
+//     const carId = req.params.id;
   
-    database.query("DELETE FROM cars WHERE id = ?", [carId], (err, result) => {
-      if (err) throw err;
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ success: false, message: "Car not found" });
-      }
-      res.json({
-        success: true,
-        message: "Car deleted successfully",
-      });
-    });
-  });
+//     database.query("DELETE FROM cars WHERE id = ?", [carId], (err, result) => {
+//       if (err) throw err;
+//       if (result.affectedRows === 0) {
+//         return res.status(404).json({ success: false, message: "Car not found" });
+//       }
+//       res.json({
+//         success: true,
+//         message: "Car deleted successfully",
+//       });
+//     });
+//   });
   
 
-app.get("/api/v1/users", (req, res)=>{
-    database.query("SELECT * FROM user", (err, result)=>{
-        if(err) throw err;
-        res.json({
-            success: true,
-            message:"success",
-            status: 200,
-            data: result,
-        });
-    });
-});
+// app.get("/api/v1/users", (req, res)=>{
+//     database.query("SELECT * FROM user", (err, result)=>{
+//         if(err) throw err;
+//         res.json({
+//             success: true,
+//             message:"success",
+//             status: 200,
+//             data: result,
+//         });
+//     });
+// });
 
-app.post("/api/v1/login", (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required" });
-    }
+// app.post("/api/v1/login", (req, res) => {
+//     const { username, password } = req.body;
+//     if (!username || !password) {
+//         return res.status(400).json({ message: "Username and password are required" });
+//     }
 
-    database.query("SELECT * FROM user WHERE username = ?", [username], (err, result) => {
-        if (err) throw err;
-        if (result.length === 0) {
-            return res.status(401).json({ message: "Akun Tidak Ditemukan" });
-        }
+//     database.query("SELECT * FROM user WHERE username = ?", [username], (err, result) => {
+//         if (err) throw err;
+//         if (result.length === 0) {
+//             return res.status(401).json({ message: "Akun Tidak Ditemukan" });
+//         }
 
-        const user = result[0];
-        // Compare plain text password
-        if (password !== user.password) {
-            return res.status(401).json({ message: "Password Salah" });
-        }
+//         const user = result[0];
+//         // Compare plain text password
+//         if (password !== user.password) {
+//             return res.status(401).json({ message: "Password Salah" });
+//         }
 
-        const token = jwt.sign({ username }, 'your_secret_key', { expiresIn: '1h' });
+//         const token = jwt.sign({ username }, 'your_secret_key', { expiresIn: '1h' });
 
-        res.json({
-            success: true,
-            message: "Login successful",
-            status: 200,
-            data: { token },
-        });
-    });
-});
+//         res.json({
+//             success: true,
+//             message: "Login successful",
+//             status: 200,
+//             data: { token },
+//         });
+//     });
+// });
 
 const port = process.env.PORT || 5000;
 
