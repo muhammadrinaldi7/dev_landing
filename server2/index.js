b6da2f8ee9aaaf8b988fcba0320bd0ddc881ebe0
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-05-24 11:47:55 +0800

    Perubahan pada index listen port

--- server2/index.js
+++ server2/index.js
@@ -14,8 +14,7 @@ app.use(express.static('public'));
 app.use(CarsRoute);
 app.use(UsersRoute);
 
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000..");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}..`);
 });
-
