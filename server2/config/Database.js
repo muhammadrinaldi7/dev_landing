6118f47b55df9a0b8fd7f1ade5993df144fd5f53
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-05-24 13:30:36 +0800

    ubah env

--- server2/config/Database.js
+++ server2/config/Database.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
 });
+db.connect(err => {
+  if(err) throw "error koneksi",err;
+  console.log('success konek');
+})
 
 export default db;
 
