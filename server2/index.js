06d3d44395057cf7fd628001ce530b59b30c8baa
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-05-24 16:35:15 +0800

    kk

--- server2/index.js
+++ server2/index.js
@@ -10,12 +10,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-  next(); 
-});
 app.use(express.json());
 app.use(fileUpload());
 
