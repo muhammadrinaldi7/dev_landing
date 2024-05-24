2bbc68cdbfde617117936cb0fd411bfdf452bab9
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-05-24 11:38:08 +0800

    Perubahan settingan env

--- server2/config/Database.js
+++ server2/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_mbnew", "root", "", {
-  host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
